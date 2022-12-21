5aa3866b491dbf59c53e860f755ef1911db49997
brensley <brensley.foster@gmail.com>
2022-12-21 17:50:08 -0500

    reinserted build information

--- build.sh
+++ build.sh
@@ -11,5 +11,9 @@ then
   echo "Branch is dev"
 else
   echo "Branch is sandbox"
-  sudo docker build -t sample_container_prod -f containers/sample_container/dockerfile .
+   sudo docker build 
+  --build-arg AWS_KEY=$AWS_ACCESS_KEY_ID 
+  --build-arg AWS_SECRET_KEY=$AWS_SECRET_ACCESS_KEY 
+  --build-arg env_type=sandbox 
+  -t sample_container_prod -f containers/sample_container/dockerfile .
 fi
