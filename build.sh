27bef350c14326a8c792924627031a979f680705
brensley <brensley.foster@gmail.com>
2022-12-21 15:20:13 -0500

    removed key info

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ then
 else
   echo "Branch is sandbox"
   sudo docker build 
-  --build-arg AWS_KEY=AKIAZNNB6QIABDQETC67
-  --build-arg AWS_SECRET_KEY=buL1LI1QHZJs08hS4Xvd+ep1NS4eclrtTvSrpHth
+  --build-arg AWS_KEY=
+  --build-arg AWS_SECRET_KEY=
   --build-arg env_type=sandbox 
   -t sample_container_prod -f containers/sample_container/dockerfile .
 fi
